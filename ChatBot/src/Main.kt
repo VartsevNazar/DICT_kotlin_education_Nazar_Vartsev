6eae30d5ba07c23a92431fd25ad8bca55d6b5322
Nazar Vartsev <gullavraq@gmail.com>
2024-02-14 06:33:06 +0200

    update: prints a greeting and prompts for a username

---
+++ ChatBot/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("""
+        Hello! My name is DICT_Bot.
+        Please, remind me your name.
+    """.trimIndent())
+    val stringUsername = readln()
+    println("What a great name you have, $stringUsername!")
+}
