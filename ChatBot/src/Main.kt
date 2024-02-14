86e02fa1b30328e653c1d9cb6ae57aca411f047f
VartsevNazar <gullavraq@gmail.com>
2024-02-14 09:30:36 +0200

    update: added comments

--- ChatBot/src/Main.kt
+++ ChatBot/src/Main.kt
@@ -1,10 +1,12 @@
 fun main() {
+    // Greeting message
     println("""
         Hello! My name is DICT_Bot.
         Please, remind me your name.
     """.trimIndent())
     val stringUsername = readln()
     println("What a great name you have, $stringUsername!")
+    // Age calculation
     println("""
         Let me guess your age.
         Enter remainders of dividing your age by 3, 5 and 7.
@@ -14,6 +16,7 @@ fun main() {
     val remainder7 = readln().toInt()
     val age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105  // user age calculation
     println("Your age is $age!")
+    // Account from 0 to number
     println("Now I will prove to you that I can count to any number you want.")
     val number = readln().toInt()
     for (i in 0..number) println("$i!")
