853945d1c670b28823e3c61f06fc1605a5a1af53
VartsevNazar <gullavraq@gmail.com>
2024-02-14 07:55:28 +0200

    update: added calculation of user age using formula

--- ChatBot/src/Main.kt
+++ ChatBot/src/Main.kt
@@ -5,4 +5,13 @@ fun main() {
     """.trimIndent())
     val stringUsername = readln()
     println("What a great name you have, $stringUsername!")
+    println("""
+        Let me guess your age.
+        Enter remainders of dividing your age by 3, 5 and 7.
+    """.trimIndent())
+    val remainder3 = readln().toInt()
+    val remainder5 = readln().toInt()
+    val remainder7 = readln().toInt()
+    val age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105  // user age calculation
+    println("Your age is $age!")
 }
