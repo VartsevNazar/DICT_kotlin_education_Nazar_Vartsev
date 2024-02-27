f874db127248fdbd1ba8a229e701fdffb964ac42
VartsevNazar <gullavraq@gmail.com>
2024-02-28 00:20:04 +0200

    update: Stage {1, 2}

--- Practice_2/src/Main.kt
+++ Practice_2/src/Main.kt
@@ -1,3 +1,13 @@
 fun main() {
-
+    // Stage 1, 2
+    var number = readln().toInt()
+    when {
+        number % 2 == 0 -> {
+            println("Even")
+            number *= 4
+        } else -> {
+            println("Odd")
+            number *= 5
+        }
+    }
 }
