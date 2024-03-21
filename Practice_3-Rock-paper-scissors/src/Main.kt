6f47e21288d48e3f78634383d738e737f7818352
VartsevNazar <gullavraq@gmail.com>
2024-03-21 07:12:53 +0200

    created: fun printGreetings

--- Practice_3-Rock-paper-scissors/src/Main.kt
+++ Practice_3-Rock-paper-scissors/src/Main.kt
@@ -1,3 +1,10 @@
-fun main(){
+fun printGreetings() {
+    println("""
+        Це гра Камінь-ножиці-папір.
+        Введіть камінь, ножиці або папір:
+    """.trimIndent())
+}
 
+fun main(){
+    printGreetings()
 }
