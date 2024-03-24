8b10d125bc6a99bf2842df4ddba76b96d377d788
VartsevNazar <gullavraq@gmail.com>
2024-03-24 18:09:19 +0200

    update: created fun exitProcessViaNull, entering data for the matrix

--- Practice_4-Cinema/src/Main.kt
+++ Practice_4-Cinema/src/Main.kt
@@ -1,3 +1,24 @@
-fun main() {
+import kotlin.system.exitProcess
 
+fun exitProcessViaNull(string: String?) {
+    if (string == null) {
+        println("Data entry completed")
+        exitProcess(0)
+    }
+}
+
+fun main() {
+    println("Enter the number of rows:")
+    var n = readlnOrNull()
+    exitProcessViaNull(n)
+    val numberRows = try { n!!.toInt() } catch (e: NumberFormatException) {
+        println("Must be a number\n")
+    }
+
+    println("Enter the number of seats in each row:")
+    n = readlnOrNull()
+    exitProcessViaNull(n)
+    val seatsInRow = try { n!!.toInt() } catch (e: NumberFormatException) {
+        println("Must be a number\n")
+    }
 }
